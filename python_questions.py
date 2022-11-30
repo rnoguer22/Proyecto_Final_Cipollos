d5eecef77dc63a2fb8707eca889e4e198555fe18
rnoguer22 <rnogugon@myuax.com>
2022-11-30 12:06:17 +0100

    Completamos word preguntas hasta la 8º

--- python_questions.py
+++ python_questions.py
@@ -1,5 +1,8 @@
 import pandas as pd
 
-df = pd.read_csv("regression_data.csv", sep=";")
-
+#Preunta 1
+df = pd.read_excel("Files/regression_data.xlsx")
 print(df)
+
+#Preginta2
+df.drop(columns="date", inplace=True)
